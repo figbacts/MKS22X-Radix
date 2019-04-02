b63af266adcea432e7104cb6114e3b369773bb33
Jack Lam <jlam00@stuy.edu>
2019-04-02 11:45:04 -0400

    f

---
+++ Radix.java
@@ -0,0 +1,16 @@
+import java.util.*;
+@SuppressWarnings("unchecked")
+public class Radix{
+   public static void radixsort(int[]data){ // if length of bucket 0 + buctket 10  == length of data STOP
+     MyLinkedList[]Buckets = new MyLinkedList[20];
+     MyLinkedList zero = new MyLinkedList();
+     Buckets[0] = zero;
+     MyLinkedList one = new MyLinkedList();
+     Buckets[1] = one;
+      }
+   public static void main(String[] args) {
+     int[] data = {0,1,2,3,4,5,6,7,8,9};
+     radixsort(data);
+
+   }
+}
