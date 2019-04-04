c5861062a352bca43c0b568345d337ad9cb064f8
Jack Lam <jlam00@stuy.edu>
2019-04-04 00:01:44 -0400

    got almosted the basic of radix sort done

--- Radix.java
+++ Radix.java
@@ -2,7 +2,7 @@ import java.util.*;
 @SuppressWarnings("unchecked")
 public class Radix{
    public static void radixsort(int[]data){ // if length of bucket 0 + buctket 10  == length of data STOP
-     MyLinkedList[]Buckets = new MyLinkedList[20];
+     MyLinkedList []Buckets = new MyLinkedList[10];
      MyLinkedList zero = new MyLinkedList();
      Buckets[0] = zero;
      MyLinkedList one = new MyLinkedList();
@@ -10,10 +10,93 @@ public class Radix{
      MyLinkedList two = new MyLinkedList();
      Buckets[2] = two;
      MyLinkedList three = new MyLinkedList();
-     Buckets[3] = three;     
+     Buckets[3] = three;
+     MyLinkedList four = new MyLinkedList();
+     Buckets[4] = four;
+     MyLinkedList five = new MyLinkedList();
+     Buckets[5] = five;
+     MyLinkedList six = new MyLinkedList();
+     Buckets[6] = six;
+     MyLinkedList seven = new MyLinkedList();
+     Buckets[7] = seven;
+     MyLinkedList eight = new MyLinkedList();
+     Buckets[8] = eight;
+     MyLinkedList nine = new MyLinkedList();
+     Buckets[9] = nine;
+     MyLinkedList ary = new MyLinkedList();
+     for(int i = 0; i < data.length; i ++){
+       ary.add(data[i]);
+     }
+     //System.out.println(ary);
+     int power = 1;
+     int counter = 0;
+     int times = 0;
+     Object holder;
+     //while(Buckets[0].size() != data.length){
+      // for(int k =0 ; k <10; k ++){
+      //   Buckets[k].clear();
+       //}
+       while (times != 2){
+       while (counter != 10){
+         holder = ary.removeFront();
+        if (((int)holder / power)% 10 == 0){
+          Buckets[0].add(holder);
+          System.out.println(Buckets[0]);
+        }
+        if (((int)holder / power)% 10 == 1){
+          Buckets[1].add(holder);
+          System.out.println(Buckets[1]);
+        }
+        if (((int)holder / power)% 10 == 2){
+          Buckets[2].add(holder);
+          System.out.println(Buckets[2]);
+        }
+        if (((int)holder / power)% 10 == 3){
+          Buckets[3].add(holder);
+          System.out.println(Buckets[3]);
+        }
+        if (((int)holder / power)% 10 == 4){
+          Buckets[4].add(holder);
+          System.out.println(Buckets[4]);
+        }
+        if (((int)holder / power)% 10 == 5){
+          Buckets[5].add(holder);
+          System.out.println(Buckets[5]);
+        }
+        if (((int)holder / power)% 10 == 6){
+          Buckets[6].add(holder);
+          System.out.println(Buckets[6]);
+        }
+        if (((int)holder / power)% 10 == 7){
+          Buckets[7].add(holder);
+          System.out.println(Buckets[7]);
+        }
+        if (((int)holder / power)% 10 == 8){
+          Buckets[8].add(holder);
+          System.out.println(Buckets[8]);
+        }
+        if (((int)holder / power)% 10 == 9){
+          Buckets[9].add(holder);
+          System.out.println(Buckets[9]);
+        }
+        counter ++;
+     }
+    ary.clear();
+    for (int j =0; j < 10; j ++){
+      if (Buckets[j].size() != 0){
+        ary.extend(Buckets[j]);
+        Buckets[j].clear();
       }
+ }
+    System.out.println(ary);
+    power = power * 10;
+    times ++;
+    counter = 0;
+  //}
+}
+}
    public static void main(String[] args) {
-     int[] data = {0,1,2,3,4,5,6,7,8,9};
+     int[] data = {12,21,33,54,65,76,87,89,98,10};
      radixsort(data);
 
    }
