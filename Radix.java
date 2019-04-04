2e1fa5b27c44a649cb1e277f95b88219d381283c
Jack Lam <jlam00@stuy.edu>
2019-04-04 00:05:52 -0400

    works but not for negative numbers

--- Radix.java
+++ Radix.java
@@ -30,54 +30,58 @@ public class Radix{
      //System.out.println(ary);
      int power = 1;
      int counter = 0;
-     int times = 0;
+     //int times = 0;
      Object holder;
      //while(Buckets[0].size() != data.length){
       // for(int k =0 ; k <10; k ++){
       //   Buckets[k].clear();
        //}
-       while (times != 2){
+       while (Buckets[0].size() != data.length){
+         for(int k =0 ; k <10; k ++){
+            Buckets[k].clear();
+          }
+
        while (counter != 10){
          holder = ary.removeFront();
         if (((int)holder / power)% 10 == 0){
           Buckets[0].add(holder);
-          System.out.println(Buckets[0]);
+          //System.out.println(Buckets[0]);
         }
         if (((int)holder / power)% 10 == 1){
           Buckets[1].add(holder);
-          System.out.println(Buckets[1]);
+          //System.out.println(Buckets[1]);
         }
         if (((int)holder / power)% 10 == 2){
           Buckets[2].add(holder);
-          System.out.println(Buckets[2]);
+          //System.out.println(Buckets[2]);
         }
         if (((int)holder / power)% 10 == 3){
           Buckets[3].add(holder);
-          System.out.println(Buckets[3]);
+          //System.out.println(Buckets[3]);
         }
         if (((int)holder / power)% 10 == 4){
           Buckets[4].add(holder);
-          System.out.println(Buckets[4]);
+          //System.out.println(Buckets[4]);
         }
         if (((int)holder / power)% 10 == 5){
           Buckets[5].add(holder);
-          System.out.println(Buckets[5]);
+          //System.out.println(Buckets[5]);
         }
         if (((int)holder / power)% 10 == 6){
           Buckets[6].add(holder);
-          System.out.println(Buckets[6]);
+          //System.out.println(Buckets[6]);
         }
         if (((int)holder / power)% 10 == 7){
           Buckets[7].add(holder);
-          System.out.println(Buckets[7]);
+          //System.out.println(Buckets[7]);
         }
         if (((int)holder / power)% 10 == 8){
           Buckets[8].add(holder);
-          System.out.println(Buckets[8]);
+          //System.out.println(Buckets[8]);
         }
         if (((int)holder / power)% 10 == 9){
           Buckets[9].add(holder);
-          System.out.println(Buckets[9]);
+          //System.out.println(Buckets[9]);
         }
         counter ++;
      }
@@ -85,15 +89,15 @@ public class Radix{
     for (int j =0; j < 10; j ++){
       if (Buckets[j].size() != 0){
         ary.extend(Buckets[j]);
-        Buckets[j].clear();
+        //Buckets[j].clear();
       }
  }
-    System.out.println(ary);
+    //System.out.println(ary);
     power = power * 10;
-    times ++;
     counter = 0;
   //}
 }
+System.out.println(ary);
 }
    public static void main(String[] args) {
      int[] data = {12,21,33,54,65,76,87,89,98,10};
